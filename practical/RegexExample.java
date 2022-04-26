64c4db64beabba6da5ea171246698b971c3d9dbc
Sangeethade <102943538+Sangeethade@users.noreply.github.com>
2022-04-26 19:42:46 +0530

    Add files via upload

---
+++ practical/RegexExample.java
@@ -0,0 +1,20 @@
+package javaprograms;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+ class RegexExample {
+
+	public static void main(String[] args) {
+		Pattern p = Pattern.compile(".a");
+		Matcher m = p.matcher("ma");
+		boolean b = m.matcher();
+		
+		boolean bb = Pattern.compile("..m").matcher("aam").matches();
+		boolean b1 = Pattern.matches("[a-zA-Z0-9]{8,}","Anu123ppp");
+		boolean b2 = Pattern.matches("[6789]{1}[0-9]{9},"8348222888");
+        boolean b3 = Pattern.matches("^[a-z0-9+_.-]*@[a-zA-Z0-9]*+\\.[a-z]{3}","mamta123@gmail.com");
+		boolean b4 = Pattern.matches("^[a-zA-Z0-9+_.-]{8,}","Mamta123_.");
+		System.out.println(b+ " "+bb+" "+b1+" "+b2+" "+b3+" "+b4);
+	}
+
+}
