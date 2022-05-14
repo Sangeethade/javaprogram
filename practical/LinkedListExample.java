9317225a24e6737c9dbc377dafd15cd1bd589b78
Sangeethade <102943538+Sangeethade@users.noreply.github.com>
2022-05-14 20:56:22 +0530

    Add files via upload

---
+++ practical/LinkedListExample.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		LinkedList<String> al = new LinkedList<String>();
+		al.add("Ram");
+		al.add("Sam");
+		al.add("Ravi");
+		al.add("Ram");
+		al.add("Rahul");
+		al.add(0,"Arjun");
+		al.addFirst("Mamta");
+		al.add("Priya");
+		al.addLast("Geeta");
+
+		Iterator<String> itr = al.iterator();
+		while (itr.hasNext()) {
+		System.out.println(itr.next());
+		}
+
+		al.removeLast();
+		al.removeFirst();
+		al.remove(2);
+		System.out.println("List after removal");
+		Iterator<String> itr1 = al.iterator();
+		while (itr1.hasNext()) {
+		System.out.println(itr1.next());
+		}
+		System.out.println("List in reverse order");
+		Iterator<String> itrr = al.descendingIterator();
+		while (itrr.hasNext()) {
+		System.out.println(itrr.next());
+		
+	}
+
+	}
+}
