7fce10f4db900fdbf977f52b1d5429bdb9896397
Sangeethade <102943538+Sangeethade@users.noreply.github.com>
2022-07-15 18:43:11 +0530

    Add files via upload

---
+++ lab activity/com.vehicle/bike.java
@@ -0,0 +1,7 @@
+
+public class Bike extends Vehicle{
+
+public Bike(boolean engine, boolean wheels, String name, String type) {
+super(engine, wheels, name, type);	
+}
+}
---
+++ lab activity/com.vehicle/car.java
@@ -0,0 +1,5 @@
+public class Car extends Vehicle {
+
+public Car(boolean engine, boolean wheels, String name, String type) {
+super(engine, wheels, name, type);	
+}
