067718086f8ef71e65df106f561384954c6a340c
Sangeethade <102943538+Sangeethade@users.noreply.github.com>
2022-07-29 20:13:20 +0530

    Delete Sprint1Project directory

--- Sprint1Project/Sprint1Project/src/main/java/com/Dao/EmpFreshDao.java
+++
@@ -1,9 +0,0 @@
-package com.Dao;
-
-public interface EmpFreshDao {
-	public void CreateEmp();
-	public void UpdateEmp();
-	public void DeleteEmp();
-	public void ShowEmp();
-
-}
--- Sprint1Project/Sprint1Project/src/main/java/com/Dao/EmpSalDao.java
+++
@@ -1,9 +0,0 @@
-package com.Dao;
-
-public interface EmpSalDao {
-	public void CreateSal();
-	public void UpdateSal();
-	public void DeleteSal();
-	public void ShowSal();
-
-}
